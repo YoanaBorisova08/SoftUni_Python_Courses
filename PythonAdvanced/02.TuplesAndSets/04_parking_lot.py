e0f05804291a605854e90fc59dcc9d12bf4a055b
Yoana Borisova <yoanasb08@gmail.com>
2025-09-15 16:40:05 +0300

    commit changes

---
+++ PythonAdvanced/02.TuplesAndSets/04_parking_lot.py
@@ -0,0 +1,11 @@
+n = int(input())
+guests = set()
+for _ in range(n):
+    guests.add(input())
+while (guest:=input()) != "END":
+    if guest in guests:
+        guests.remove(guest)
+print(len(guests))
+sorted_guests = sorted(guests)
+for guest in sorted_guests:
+    print(guest)
